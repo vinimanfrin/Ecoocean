fb0f33911dfaa301ad5503e5ac868a571e116d5f
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-06-04 16:18:36 -0300

    FIX: apenas usuarios administradores têm acesso ao recursos de VoluntariosAdmin

--- src/main/java/com/gs/ecoocean/controller/VoluntarioAdminController.java
+++ src/main/java/com/gs/ecoocean/controller/VoluntarioAdminController.java
@@ -12,11 +12,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RestController
 @RequestMapping("/voluntarios-admin")
+@PreAuthorize("hasRole('ADMIN')")
 public class VoluntarioAdminController {
 
     @Autowired
