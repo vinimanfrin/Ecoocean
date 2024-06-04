a3e432e40f90ea6a2943505b021603f2c8cbd346
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-06-04 16:40:04 -0300

    FIX: apenas administradores podem deletar usuários

--- src/main/java/com/gs/ecoocean/controller/VoluntarioController.java
+++ src/main/java/com/gs/ecoocean/controller/VoluntarioController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
@@ -48,6 +49,7 @@ public class VoluntarioController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize(value = "hasRole('ADMIN')")
     public ResponseEntity<Void> deleteById(@PathVariable Long id){
         service.deleteById(id);
         return ResponseEntity.noContent().build();
